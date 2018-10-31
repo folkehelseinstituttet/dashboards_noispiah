9c43d1ca53942bf7ba706cc773937d9912a5dedc
Richard White <r@rwhite.no>
2018-10-31 12:32:43 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,64 @@
 #!/bin/bash
-set -o errexit -o nounset
-PKG_REPO=$PWD
-cd ..
+
+# see https://github.com/travis-ci/travis-ci/issues/1701
+function travis-branch-commit() {
+    local head_ref branch_ref
+    head_ref=$(git rev-parse HEAD)
+    if [[ $? -ne 0 || ! $head_ref ]]; then
+        err "failed to get HEAD reference"
+        return 1
+    fi
+    branch_ref=$(git rev-parse "$TRAVIS_BRANCH")
+    if [[ $? -ne 0 || ! $branch_ref ]]; then
+        err "failed to get $TRAVIS_BRANCH reference"
+        return 1
+    fi
+    if [[ $head_ref != $branch_ref ]]; then
+        msg "HEAD ref ($head_ref) does not match $TRAVIS_BRANCH ref ($branch_ref)"
+        msg "someone may have pushed new commits before this build cloned the repo"
+        return 0
+    fi
+    if ! git checkout "$TRAVIS_BRANCH"; then
+        err "failed to checkout $TRAVIS_BRANCH"
+        return 1
+    fi
+
+    if ! git add --all .; then
+        err "failed to add modified files to git index"
+        return 1
+    fi
+    # make Travis CI skip this build
+    if ! git commit -m "Travis CI update [$TRAVIS_BUILD_NUMBER]"; then
+        err "failed to commit updates"
+        return 1
+    fi
+    # add to your .travis.yml: `branches\n  except:\n  - "/\\+travis\\d+$/"\n`
+    #local git_tag=SOME_TAG_TRAVIS_WILL_NOT_BUILD+travis$TRAVIS_BUILD_NUMBER
+    #if ! git tag "$git_tag" -m "Generated tag from Travis CI build $TRAVIS_BUILD_NUMBER"; then
+    #    err "failed to create git tag: $git_tag"
+    #    return 1
+    #fi
+    local remote=origin
+    if [[ $GH_TOKEN ]]; then
+        remote=https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG
+    fi
+    if [[ $TRAVIS_BRANCH != master ]]; then
+        msg "not pushing updates to branch $TRAVIS_BRANCH"
+        return 0
+    fi
+    if ! git push --quiet --follow-tags "$remote" "$TRAVIS_BRANCH" > /dev/null 2>&1; then
+        err "failed to push git changes"
+        return 1
+    fi
+}
+
+function msg() {
+    echo "travis-commit: $*"
+}
+
+function err() {
+    msg "$*" 1>&2
+}
 
 addToDrat(){
   mkdir drat; cd drat
@@ -26,11 +83,15 @@ addToDrat(){
 
 }
 
+set -o errexit -o nounset
+PKG_REPO=$PWD
+cd ..
+
 addToDrat
 
-## Other options:
-## Only add if the commit is tagged: so something like:
-#if [ $TRAVIS_TAG ] ; then
-#   addToDrat
-#fi
-##but will need to edit .travis.yml since $TRAVIS_BRANCH will now equal $TRAVIS_TAG
+rm $PKG_REPO/$PKG_TARBALL
+cd $PKG_REPO
+
+Rscript -e "styler::style_pkg('$PKG_REPO/')"
+
+travis-branch-commit 
